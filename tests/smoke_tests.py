53eb99ca2e56caf9b4d2e6c78fa9341aa73a7ab1
obenfeto <obenfeto@yahoo.fr>
2025-07-31 11:23:55 -0400

    change base URL to lab06

--- tests/smoke_tests.py
+++ tests/smoke_tests.py
@@ -1,6 +1,6 @@
 import requests
 
-BASE_URL = "https://finapi-solution-lab05.azurewebsites.net" # <------ Replace with your actual base URL
+BASE_URL = "https://finapi-solution-lab06.azurewebsites.net" # <------ Replace with your actual base URL
 
 def test_home():
     r = requests.get(f"{BASE_URL}/")
